870abadeb592c5f57a7487d3570003d4b5940c38
mathiasomar <mathiasomar@gmail.com>
2026-01-21 11:35:25 +0300

    Change sidebar settings

--- app/(dashboard)/dashboard/organizations/data-table.tsx
+++ app/(dashboard)/dashboard/organizations/data-table.tsx
@@ -86,7 +86,7 @@ export function DataTable<TData, TValue>({
                       ? null
                       : flexRender(
                           header.column.columnDef.header,
-                          header.getContext()
+                          header.getContext(),
                         )}
                   </TableHead>
                 );
