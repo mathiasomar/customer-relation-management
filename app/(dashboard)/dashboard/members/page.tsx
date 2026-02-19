6cae96db2949462f7f7e0af6477790e7c64d7f73
mathiasomar <mathiasomar@gmail.com>
2026-02-19 12:38:32 +0300

    Remove search

--- app/(dashboard)/dashboard/members/page.tsx
+++ app/(dashboard)/dashboard/members/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import InviteMember from "@/components/dashboard/invite-member";
-import Search from "@/components/dashboard/search";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+// import Search from "@/components/dashboard/search";
 import { useTenant } from "@/hooks/use-tenant";
 // import ViewDataTable from "./view-data-table";
 import ViewMembers from "./view-members";
@@ -19,26 +11,6 @@ const MembersPage = () => {
   return (
     <div className="w-full space-y-6">
       <div className="w-full flex flex-col gap-2 md:flex-row md:items-center md:justify-between">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard/organizations/view">
-                {isFetching
-                  ? "Loading..."
-                  : data?.tenant?.slug || "Unknown Tenant"}
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Members</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-
         {/* Buttons */}
         <div className="flex items-center-safe gap-2">
           {data?.userRole === "ADMIN" || data?.userRole === "MANAGER" ? (
@@ -54,9 +26,7 @@ const MembersPage = () => {
       </div>
 
       {/* Table here */}
-      <div className="my-4">
-        <Search />
-      </div>
+      <div className="my-4">{/* <Search /> */}</div>
       {/* <ViewDataTable /> */}
       {/* Members cards */}
       <ViewMembers />
